7d96d7143b26f0215919eb006a61ca2dec0c07fd
Irza Pulungan <irzaip@gmail.com>
2017-02-27 22:45:56 +0700

    no bug fixing

--- speech/cipi_sr.py
+++ speech/cipi_sr.py
@@ -14,16 +14,30 @@ interrupted = False
 yesno_state = False
 listen_state = False
 talk_state = False
+sleep_state = False
 cmd = ""
+prm = ""
 
-if cmd == "time":
-    a = datetime.datetime.now()
-    hour = a.time().hour
-    minute = a.time().minute
-    resp = "Sekarang jam " + str(hour)+ " lewat " + str(minute)
-
+def command(cmd,prm):
+    global talk_state, listen_state, sleep_state ,yesno_state
+    if cmd == "JAM":
+        a = datetime.datetime.now()
+        hour = a.time().hour
+        if hour > 12:
+            hour = hour - 12
+        minute = a.time().minute
+        resp = "Sekarang jam %s lewat %s" % (hour, minute)
+        return resp
+    if cmd == "TIDUR":
+        sleep_state = True
+        yesno_state = True
+        return "Sekarang bobo"
+    if cmd == "LAMPOFF":
+        resp = "oke, telah dimatikan"
+        return resp
 
 def cek_talking():
+    global talk_state, listen_state, sleep_state ,yesno_state
     try:
         m = browser.find_element_by_class_name("goog-toolbar-button-checked")
         talk_state = True
@@ -33,35 +47,53 @@ def cek_talking():
         return False
 
 def get_speech():
-    if talk_state == True: time.sleep(2)
-    listen_state = True
-    record = speech_rec.find_element_by_xpath('//*[@id="gt-speech"]/span')
-    record.click()
-    time.sleep(4)
-    #record
-    record.click()
-    source = speech_rec.find_element_by_xpath('//*[@id="source"]')
-    a = source.get_attribute("value")
-    print a
-    listen_state = False
-    return a
+    global talk_state, listen_state, sleep_state ,yesno_state
+    if not talk_state:
+        if not sleep_state:
+            listen_state = True
+            source = browser.find_element_by_xpath('//*[@id="source"]')
+            source.clear()
+            record = browser.find_element_by_xpath('//*[@id="gt-speech"]/span')
+            record.click()
+            time.sleep(5)
+            #record
+            record.click()
+            a = source.get_attribute("value")
+            print a
+            listen_state = False
+            source.clear()
+            return a
 
 def talk(speech):
-    if listen_state == True: time.sleep(4)
-    talk_state = True
-    tts = browser.find_element_by_xpath('//*[@id="source"]')
-    tts.clear()
-    tts.send_keys(str(speech))
-    time.sleep(2)
-    talk = browser.find_element_by_xpath('//*[@id="gt-src-listen"]/span')
-    talk.click()
-    talk_state = False
+    global talk_state, listen_state, sleep_state ,yesno_state, cmd, prm
+    if not talk_state:
+        if not sleep_state:
+            try:
+                talk_state = True
+                tts = browser.find_element_by_xpath('//*[@id="source"]')
+                tts.clear()
+                tts.send_keys(str(speech))
+                time.sleep(1)
+                talk = browser.find_element_by_xpath('//*[@id="gt-src-listen"]/span')
+                talk.click()
+                time.sleep(2)
+                talk_state = False
+            except:
+                pass
 
 def find_cmd(response):
+    global talk_state, listen_state, sleep_state ,yesno_state, cmd, prm
     m = re.search(r'CMD:(\S+)\sPRM:(\S+)]',response)
     if m: 
-        print m.groups()
-        response = response[0:response.find("[")]+response[response.find("]")+1:]
+        cmd = m.groups(1)[0]
+        prm = m.groups(1)[1]
+        print "CMD: %s , PRM: %s" % (cmd,prm)
+        #
+        #response = response[0:response.find("[")]+response[response.find("]")+1:]
+        #response = response + " " + str(command(cmd,prm))
+        response = command(cmd,prm)
+    else:
+        response = response
     return response
 
 def signal_handler(signal, frame):
@@ -73,25 +105,27 @@ def interrupt_callback():
     return interrupted
 
 def play_ding():
-    if not cek_talking():
-        print "Play ding"
-        snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING)
-        respn = get_speech()
-        spik = find_cmd(a.respond(respn))
-        talk(spik)
+    global sleep_state, yesno_state
+    if not yesno_state:
+        if not sleep_state: 
+            if not cek_talking():
+                print "Play ding"
+                snowboydecoder.play_audio_file(snowboydecoder.DETECT_DING)
+                respn = get_speech()
+                print "respn:",respn
+                spik = find_cmd(a.respond(respn))
+                print "spik:",spik
+                talk(spik)
 
 def yes_resp():
     print "He said: YES"
     if yesno_state: snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)
 
 def no_resp():
+    global talk_state, listen_state, sleep_state ,yesno_state
     print "He said: NO"
     if yesno_state: snowboydecoder.play_audio_file(snowboydecoder.DETECT_DONG)
 
-'''if len(sys.argv) != 4:
-    print("Error: need to specify 2 model names")
-    print("Usage: python demo.py 1st.model 2nd.model")
-    sys.exit(-1)'''
 
 models = ['cipi.pmdl','ya.pmdl','enggak.pmdl']
 
@@ -107,16 +141,17 @@ callbacks = [lambda: play_ding(),
 print('Listening... Press Ctrl+C to exit')
 
 
-browser = webdriver.Chrome()
-browser.get('https://translate.google.com/?#id/en/apa%20kabar')
-assert "Google Translate" in browser.title
-
 chrome_options = Options()
 chrome_options.add_argument("--use-fake-ui-for-media-stream")
 
-speech_rec = webdriver.Chrome(chrome_options=chrome_options)
-speech_rec.get('https://translate.google.com/?#id/en')
-assert "Google Translate" in speech_rec.title
+browser = webdriver.Chrome(chrome_options=chrome_options)
+browser.get('https://translate.google.com/?#id/en/apa%20kabar')
+assert "Google Translate" in browser.title
+talk("Halo, cipi siap menunggu perintah")
+
+#speech_rec = webdriver.Chrome(chrome_options=chrome_options)
+#speech_rec.get('https://translate.google.com/?#id/en')
+#assert "Google Translate" in speech_rec.title
 
 
 a = aiml.Kernel()
